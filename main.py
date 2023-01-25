20a5003a76f2abe0a47217bfbe6a7c4ef191802e
Jansen Lima <jansen.lima.df@gmail.com>
2023-01-24 23:14:56 -0300

    Adicionado metodo GET para cursos e curso

--- main.py
+++ main.py
@@ -14,7 +14,20 @@ cursos = {
         "horas": 47,
     },
 }
+
+@app.get("/cursos")
+async def get_cursos():
+    return cursos
+
+@app.get("/cursos/{curso_id}")
+async def get_curso(curso_id: int):
+    curso = cursos[curso_id]
+    curso.update({"id": curso_id})
+
+    return curso
+
+
 if __name__ == '__main__':
     import uvicorn
 
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, debug=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)

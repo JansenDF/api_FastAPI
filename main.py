91f2aa7b8c7220fbf811acddde89290d7e78b505
Jansen Lima <jansen.lima.df@gmail.com>
2023-01-25 21:50:47 -0300

    Adicionado metodo POST para criar curso

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 from fastapi import FastAPI, HTTPException, status
 
+from models import Curso
+
+
 app = FastAPI()
 
 cursos = {
@@ -27,6 +30,13 @@ async def get_curso(curso_id: int):
     except KeyError:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='Curso não encontrado.')
 
+@app.post('/cursos', status_code=status.HTTP_201_CREATED)
+async def post_curso(curso: Curso):
+    next_id = len(cursos) + 1
+    cursos[next_id] = curso
+    del curso.id
+    return curso
+    
 
 if __name__ == '__main__':
     import uvicorn
